a133a5dee43cd480408e520b10a4e0b187de1c1d
French <77544281+french08@users.noreply.github.com>
2023-04-18 18:31:39 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
-
 module.exports = {
 
   Prefix: ",", // PREFIX DU BOT.
 
   Users: {
-    OWNERS: ["699570257869799455","770232737704968222","834391822423556117"] // L'ID DES PROPRIÉTAIRES DU BOT.
+    OWNERS: ["Vous devrez mettre votre id"] // L'ID DES PROPRIÉTAIRES DU BOT.
   },
   
   Permissions: {
@@ -12,12 +11,12 @@ module.exports = {
   },
   
   Client: {
-    TOKEN: "ODM1NTA5NTEyMDE3NTQzMTY4.GV2Bsr.Elgnx3TZ7ROd3-dAn8zLpaSIp9qX2FA_uXDQBc", // VOTRE TOKEN DE BOT. (UTILISEZ CELA UNIQUEMENT DANS VSCODE)
-    ID: ["835509512017543168"] // ID DE BOT.
+    TOKEN: "Le token du bot", // VOTRE TOKEN DE BOT.
+    ID: ["L'id du bot"] // ID DE BOT.
   },
 
   couleurs: {
-    rouge: "Red",
+    rouge: "Red", 
     bleu: "Blue",
     vert: "Green",
     jaune: "Yellow"
